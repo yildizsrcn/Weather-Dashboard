0925bfe12fa291827a72d302684db4c07471674c
Yusuf Sercan YILDIZ <srcnyildiz@Stars-MacBook-Pro.local>
2024-02-01 17:48:05 +0000

    js updated

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,12 @@ var todayElement= $("#today");;
 var forecastElement= $("#forecast");;
 var city= [];
 var currentDay= day.js();
-
+var history= $("#history");
+var queryUrl;
+var apiKey= ['612116ca2c140583852608ede5b4a64f']
 
 function showWeather(){
-
+queryUrl=`api.openweathermap.org/data/2.5/forecast?q={city name}&appid={}`
 
 }
 
