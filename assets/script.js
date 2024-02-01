729dd53606c530df8b92e68df14b3790d946ef2d
Yusuf Sercan YILDIZ <srcnyildiz@Stars-MacBook-Pro.local>
2024-02-01 17:18:13 +0000

    Js file updated

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var dateElement= $("#date");
+var timeElement= $("#time");;
+var todayElement= $("#today");;
+var forecastElement= $("#forecast");;
+var city= [];
+var currentDay= day.js();
+
+
+function showWeather(){
+
+
+}
+
+
+
